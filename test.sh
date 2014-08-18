264b1163d29412a70233a0e97498f7a65e2f5606
Alexander Nadeau <wareya@gmail.com>
2014-08-18 11:32:24 -0400

    add optimization settings to test.sh

--- test.sh
+++ test.sh
@@ -5,10 +5,17 @@ sdliflags='`pkg-config SDL2_image --cflags`'
 sdllflags='`pkg-config SDL2_image --libs`'
 sdllflagsstatic='`pkg-config SDL2_image --static-libs`'
 cflags="-std=c++11 -Wall -pedantic -Iinclude $sdliflags"
-pflags='-O3 -D B_FRAMELIMIT_DISABLE -D B_DEBUG_FRAMESONLY -D B_DEBUG_COREFRAMES '
-fflags='-O0 -g -ggdb'
-rflags='-O3'
+
+dflags='-O0 -g -ggdb'
+
+fflags='-O3'
+mflags='-O3 -msse -msse2' # modern x86 optimizations
+iflags='-O3 -msse -msse2 -mssse3 -msse4.1' # aggressive intel x86 optimizations
+aflags='-O3 -msse -msse2 -msse2avx' # aggressive amd x86 optimizations
+
 tflags='-D TESTS=1'
+pflags='-O3 -D B_FRAMELIMIT_DISABLE -D B_DEBUG_FRAMESONLY -D B_DEBUG_COREFRAMES '
+
 linker="-L /usr/lib -static-libstdc++ -static-libgcc $sdllflags"
 
 cmd="g++ $source $cflags $linker"
@@ -17,6 +24,12 @@ if [ "$1" == "-d" ]; then
     cmd="$cmd $dflags"
 elif [ "$1" == "-f" ]; then
     cmd="$cmd $fflags"
+elif [ "$1" == "-m" ]; then
+    cmd="$cmd $mflags"
+elif [ "$1" == "-i" ]; then
+    cmd="$cmd $iflags"
+elif [ "$1" == "-a" ]; then
+    cmd="$cmd $aflags"
 elif [ "$1" == "-p" ]; then
     cmd="$cmd $pflags"
 elif [ "$1" == "-t" ]; then
