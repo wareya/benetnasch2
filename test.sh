3b5e9e3901d533b0f62e1c0734dcc678e2c3c49c
Alexander Nadeau <wareya@gmail.com>
2014-08-26 07:59:23 -0400

    added extra stopgaps to compile script

--- test.sh
+++ test.sh
@@ -7,10 +7,18 @@ sdllflags='`sdl2-config --static-libs` -lSDL2_image -static'
 sdllflagsstatic='`pkg-config SDL2_image --static-libs` -lSDL2_image'
 cflags="-std=c++11 -Wall -pedantic -Iinclude $sdliflags -I${forceinclude}/include"
 
+if hash sdl2-config; then
+    cat /dev/null;
+else
+    echo "Could not find sdl2-config. Is SDL2 installed correctly? Aborting."
+    exit 1
+fi
+
 echo ""
-echo "Check sdl2_config --prefix:"
+echo "Check sdl2-config --prefix:"
 echo "${forceinclude}"
 echo "Please edit sdl2-config if ${forceinclude}/include/SDL2/SDL.h does not exist."
+echo "Also, if you get an 'XCClinker' error, remove that flag from sdl2_config."
 echo ""
 
 dflags='-O0 -g -ggdb -mconsole'
