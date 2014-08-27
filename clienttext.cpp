29b5b652d8283cfc41c593b48b5dfd86f6bf7ee7
Alexander Nadeau <wareya@gmail.com>
2014-08-26 22:46:02 -0400

    make server test respond to client

--- clienttext.cpp
+++ clienttext.cpp
@@ -4,11 +4,23 @@ int main()
 {
     if(faucnet_init() < 0)
         return 0;
-    auto mybuff = buffer_create();
+    auto myself_sock = udp_bind(0);
     
-    write_string(mybuff, "Hello, world!\0");
+    write_string(myself_sock, "P-please respond");
     std::cout << "CLENT: Sending message to server.\n";
-    udp_send(mybuff, "127.0.0.1", 420);
+    udp_send(myself_sock, "127.0.0.1", 420);
+    std::cout << "CLENT: Awaiting response...\n";
+    
+    auto starttime = SDL_GetTicks();
+    // wait 60 seconds for an in message on in_sock and put it into in_sock's buffer
+    while(udp_receive(myself_sock)==false and  SDL_GetTicks()-starttime < 60*1000);
+    
+    std::cout << "CLENT: Got a response!\n";
+    auto numbytes = socket_receivebuffer_size(myself_sock);
+    auto out_ip = socket_remote_ip(myself_sock);
+    auto port = socket_remote_port(myself_sock);
+    std::cout << "CLENT: Size: " << numbytes << " IP: " << out_ip << " Port: " << port << "\n";
+    std::cout << "CLENT: Message: " << read_string(myself_sock, numbytes) << "\n";
     
     faucnet_exit();
     return 0;
