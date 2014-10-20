c6ebdffc96cff96a02ade3c8f5d74466f28fd9de
Alexander Nadeau <wareya@gmail.com>
2014-10-20 15:26:36 -0400

    reorganize root

--- client.sh
+++
@@ -1,1 +0,0 @@
-g++ -march=i686 -g -ggdb -Wextra -Wall -std=c++11 clienttext.cpp -lSDL2 -o client.exe
--- server.sh
+++
@@ -1,1 +0,0 @@
-g++ -march=i686 -g -ggdb -Wextra -Wall -std=c++11 servertext.cpp -lSDL2 -o server.exe
